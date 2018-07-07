3d427f64d8d866dcc4b29eaa51e65c638955ba69
Angela Duran <duranangela19@gmail.com>
2018-07-07 08:05:33 -0600

    Add model test for astronaut, should have done this first

---
+++ spec/models/astronaut_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Astronaut, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:age)}
+    it {should validate_presence_of(:job)}
+  end
+end
